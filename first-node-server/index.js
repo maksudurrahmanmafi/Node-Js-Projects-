11b19e0947e8b6df46a116b0371d38b1c55fb634
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-08-23 13:25:00 +0600

    git ignore

--- first-node-server/index.js
+++ first-node-server/index.js
@@ -5,6 +5,9 @@ const port = 1001
 app.get('/',(req,res) =>{
     res.send("Hello I am Node Js")
 })
+app.get ('/data',(req,res) =>{
+    res.send("A data form the data route hehehehehehehehe ");
+})
 
 app.listen(port , () =>{
     console.log(`Express form the port : ${port}`);
