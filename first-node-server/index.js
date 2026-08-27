3b8929f4288cb2b2b137064f6d4b43730b25782f
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-08-27 19:27:54 +0600

    post

--- first-node-server/index.js
+++ first-node-server/index.js
@@ -1,15 +1,35 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 1001
+const express = require("express");
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 1001;
+app.use(cors())
+app.use(express.json());
 
-app.get('/',(req,res) =>{
-    res.send("Hello I am Node Js")
-})
-app.get ('/data',(req,res) =>{
-    res.send("A data form the data route hehehehehehehehe ");
+app.get("/", (req, res) => {
+  res.send("Hello I am Node Js");
+});
+app.get("/data", (req, res) => {
+  res.send("A data form the data route  ");
+});
+app.get("/jsonData", (req, res) => {
+  res.send("Json Data from json malu mafi 😂🫡 ");
+});
+
+const user = [
+    {id:1,name:"Mafi",email:"m@gamil.com"},
+    {id:2,name:"Mafi",email:"m@gamil.com"},
+    {id:3,name:"Mafi",email:"m@gamil.com"},
+]
+
+app.get('/users',(req,res)=>{
+    res.send(user);
 })
 
-app.listen(port , () =>{
-    console.log(`Express form the port : ${port}`);
+app.post('/users',(req,res)=>{
+    console.log('post method called',req.body);
     
 })
+
+app.listen(port, () => {
+  console.log(`Express form the port : ${port}`);
+});
