fc0e075ee2787ab90813be610cc93161a16be405
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-08-31 09:45:26 +0600

    Add Data from client side to server side

--- first-node-server/index.js
+++ first-node-server/index.js
@@ -1,39 +1,36 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 1001;
-app.use(cors())
+
+// Middleware
+app.use(cors());
 app.use(express.json());
 
+// Initial Data
+const users = [
+  { id: 1, name: "Mafi", email: "m@gmail.com" },
+  { id: 2, name: "Mafi", email: "m@gmail.com" },
+  { id: 3, name: "Mafi", email: "m@gmail.com" },
+];
+
+// Routes
 app.get("/", (req, res) => {
   res.send("Hello I am Node Js");
 });
-app.get("/data", (req, res) => {
-  res.send("A data form the data route  ");
-});
-app.get("/jsonData", (req, res) => {
-  res.send("Json Data from json malu mafi 😂🫡 ");
+
+app.get("/users", (req, res) => {
+  res.send(users);
 });
 
-app.get('/',(req,res) =>{
-    res.send("This is for github commit ");
-})
-
-const user = [
-    {id:1,name:"Mafi",email:"m@gamil.com"},
-    {id:2,name:"Mafi",email:"m@gamil.com"},
-    {id:3,name:"Mafi",email:"m@gamil.com"},
-]
-
-app.get('/users',(req,res)=>{
-    res.send(user);
-})
-
-app.post('/users',(req,res)=>{
-    console.log('post method called',req.body);
-    
-})
+app.post("/users", (req, res) => {
+  console.log("Post method called:", req.body);
+  const newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  res.send(newUser);
+});
 
 app.listen(port, () => {
-  console.log(`Express form the port : ${port}`);
+  console.log(`Express running on port: ${port}`);
 });
