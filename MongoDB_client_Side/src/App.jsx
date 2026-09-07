d0a04cc3fc8b35dc8a508e74fa4301e223104a7e
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-09-07 09:14:21 +0600

    User Component

--- MongoDB_client_Side/src/App.jsx
+++ MongoDB_client_Side/src/App.jsx
@@ -1,11 +1,17 @@
 
 import './App.css'
+import Users from './components/Users'
 
 function App() {
 
 
   return (
     <>
+    <h2>
+      CURD Operation
+    </h2>
+    <Users />
+
 </>
   )
 }
---
+++ MongoDB_client_Side/src/components/Users.jsx
@@ -0,0 +1,17 @@
+
+
+const Users = () => {
+    return (
+        <div>
+            <form>
+                <input type = "text" placeholder='Enter Name' />
+                <br/>
+                <input type = "text" placeholder='Enter Email' />
+                <br/>
+                <input type = "submit" value = "Submit" />
+            </form>
+        </div>
+    );
+};
+
+export default Users;
