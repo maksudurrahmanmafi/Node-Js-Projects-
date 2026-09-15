0c5402ecf97c37343002b331fb82858f27573433
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-09-15 13:43:55 +0600

    index.js

--- smart-deals-server/index.js
+++ smart-deals-server/index.js
@@ -1,39 +1,41 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 //middleware
 app.use(cors());
 app.use(express.json());
 
-const uri =  "mongodb+srv://smartdb:SmartDB2026Pass@smartdb.ld4whe7.mongodb.net/?appName=smartdb";
-const client = new MongoClient(uri,{
-    family: 4,
+const uri =
+  "mongodb+srv://smartdb:SmartDB2026Pass@smartdb.ld4whe7.mongodb.net/?appName=smartdb";
+const client = new MongoClient(uri, {
+  family: 4,
 
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-})
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 const port = process.env.PORT || 534;
-app.get("/",(req,res)=>{
-    res.send("Server is running successfully")      
-})
+app.get("/", (req, res) => {
+  res.send("Server is runningy successfully");
+});
 
-async function run  () {
-    try{
-        await client.connect();
-        console.log("Database connected successfully")
+async function run() {
+  try {
+    await client.connect();
+    console.log("Database connected successfully");
 
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    }
-    finally{
-        // await client.close();
-    }
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // await client.close();
+  }
 }
 run().catch(console.error);
-app.listen(port,(req,res)=>{
-    console.log(`Server is running on port ${port}`)
-})
+app.listen(port, (req, res) => {
+  console.log(`Server is running on port ${port}`);
+});
